0a7858d428158bda902762538e36d95c32b311a3
Kendall Strautman <kendallstraut@gmail.com>
2019-02-12 09:52:53 -0800

    config for prismic, first api query

--- src/components/RootApp.js
+++ src/components/RootApp.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import Homepage from './Homepage.js'
-
+import React from "react";
+import Homepage from "./Homepage.js";
 
 //add routing here
 
 const RootApp = () => {
-  
   return (
     <div>
-      <Homepage 
-        token = {process.env.REACT_APP_ACCESS_TOKEN}
-        spaceId = {process.env.REACT_APP_SPACE_ID}
-        baseUrl = {process.env.REACT_APP_BASE_URL}
+      <Homepage
+        token={process.env.REACT_APP_ACCESS_TOKEN}
+        clientId={process.env.REACT_APP_CLIENT_ID}
+        clientSecret={process.env.REACT_APP_CLIENT_SECRET}
+        apiEndpoint={process.env.REACT_APP_BASE_URL}
       />
     </div>
-  )
-}
+  );
+};
 
-export default RootApp
+export default RootApp;
