17b4609dd75a2f5f86e15558f0abf0de41cbddcf
Kendall Strautman <kendallstraut@gmail.com>
2019-02-12 11:21:53 -0800

    added simple header gallery functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React from "react";
 
 class Header extends React.Component {
+  handleGalleryBuild = galleryImages => {
+    if (galleryImages) {
+      return (
+        <div>
+          {galleryImages.map(image => {
+            return <img id={galleryImages.indexOf(image)} src={image} />;
+          })}
+        </div>
+      );
+    }
+  };
+
   render() {
-    const { copy , img } = this.props
+    const { copy, galleryImages } = this.props;
     return (
       <div>
-        <h1 className="header-message">{ copy }</h1>
-        <img className="header-img" src={ img } />
+        <h1 className="header-message">{copy}</h1>
+        {galleryImages
+          ? this.handleGalleryBuild(galleryImages)
+          : "https://source.unsplash.com/random"}
         <p>Exhibitions</p>
       </div>
-    )
+    );
   }
 }
 
-export default Header
+export default Header;
