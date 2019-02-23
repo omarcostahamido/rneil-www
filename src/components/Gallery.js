1c6a74dcf410fae8620f2d7ce2fe394942849547
Kendall Strautman <kendallstraut@gmail.com>
2019-02-22 16:09:57 -0800

    added gallery slider image transitions

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,15 +1,49 @@
 import React from "react";
 
+//check whether image is visible in viewport, then toggle classes
+
 class Gallery extends React.Component {
+  //STATE----------------------------------------
   state = {
-    isDesktop: false
+    isDesktop: false,
+    imageNum: 0,
+    currentX: 0,
+    images: []
+  };
+
+  //OTHER FUNCTIONS----------------------------------
+  handleGalleryScroll = () => {
+    this.handleImageAnimations(this.state.images);
+  };
+
+  handleImageAnimations = images => {
+    if (images.length > 1) {
+      // images.forEach(image => {
+      //   console.log(image);
+      //   console.log(image.getBoundingClientRect());
+      // });
+
+      images.forEach(image => {
+        image.getBoundingClientRect().left;
+        if (
+          image.getBoundingClientRect().left < window.innerWidth &&
+          image.getBoundingClientRect().left > 0
+        ) {
+          image.classList.add("is--active");
+          image.classList.remove("animate");
+        } else {
+          image.classList.add("animate");
+          image.classList.remove("is--active");
+        }
+      });
+    }
   };
 
   handleImageClick = e => {
     if (this.state.isDesktop == true) {
       document.getElementById(e.target.id.toString()).scrollIntoView({
         behavior: "smooth",
-        block: "center",
+        block: "nearest",
         inline: "center"
       });
     }
@@ -18,11 +52,14 @@ class Gallery extends React.Component {
   handleGalleryBuild = galleryImages => {
     if (galleryImages) {
       return (
-        <div className="home-header-gallery-wrap">
+        <div
+          className="home-header-gallery-wrap"
+          onScroll={this.handleGalleryScroll}
+        >
           {galleryImages.map(image => {
             return (
               <img
-                className="home-header-gallery-images"
+                className="home-header-gallery-images animate"
                 id={`home-header-gallery-image-${galleryImages.indexOf(image)}`}
                 key={`home-header-gallery-image-${galleryImages.indexOf(
                   image
@@ -37,17 +74,35 @@ class Gallery extends React.Component {
     }
   };
 
+  //LIFECYCLE--------------------------------------------------
+
   componentDidUpdate() {
     if (window.innerWidth > 1024 && this.state.isDesktop == false) {
       this.setState({
         isDesktop: true
       });
     }
+
+    if (document.querySelector("div.home-header-gallery-wrap")) {
+      let images = document.querySelectorAll("img.home-header-gallery-images");
+      // console.log(images);
+
+      if (this.state.imageNum == 0 && images.length > 1) {
+        this.setState({
+          imageNum: images.length,
+          images: images
+        });
+      }
+
+      this.handleImageAnimations(images);
+    }
   }
+
+  //RENDER----------------------------------------------------------
 
   render() {
     const { galleryImages } = this.props;
-    console.log(this.state);
+    // console.log(this.state);
     return (
       <div>
         <div className="home-header-gallery">
