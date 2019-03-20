1463c647c9da6bf9d53c37b7fd50085c2d6e1c9b
Kendall Strautman <kendallstraut@gmail.com>
2019-03-20 10:23:37 -0700

    autoplay vid hero styles & refactored to set innerhtml so muted gets passed

--- src/components/Slices/Autoplay_Video_Module.js
+++ src/components/Slices/Autoplay_Video_Module.js
@@ -1,16 +1,23 @@
 import React from "react";
 
+//had to set innerHTML so muted attribute gets passed
 const Autoplay_Video_Module = props => {
   return (
-    <div className={`media ${props.style}`}>
-      <video
-        autoPlay
-        loop
-        muted
-        src={props.autoplayVideoUrl}
-        type="video/mp4"
-      />
-    </div>
+    <div
+      className={`media ${props.style}`}
+      dangerouslySetInnerHTML={{
+        __html: `
+          <video
+            muted
+            autoplay
+            playsinline
+            loop
+            src="${props.autoplayVideoUrl}"
+            type="video/mp4"
+          />
+        `
+      }}
+    />
   );
 };
 
