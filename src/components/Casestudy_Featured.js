67f6a8212080415d682fc3dae2b78f5235e35b9b
Kendall Strautman <36613477+kendallstrautman@users.noreply.github.com>
2019-04-09 16:53:24 -0700

    Revert "Homepage Parallax - pure css"

--- src/components/Casestudy_Featured.js
+++ src/components/Casestudy_Featured.js
@@ -15,7 +15,6 @@ const Casestudy_Featured = props => {
     el.classList.add("animate");
     el.classList.remove("is--active");
   };
-
   return (
     <div>
       <Waypoint onEnter={fadeIn} onLeave={fadeOut}>
