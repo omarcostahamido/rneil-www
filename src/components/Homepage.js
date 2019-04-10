712eaa5eefda074d00d1839d811ed1b42734dec9
Kendall Strautman <kendallstraut@gmail.com>
2019-04-09 17:45:02 -0700

    converted homepage to functional component

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,68 +3,60 @@ import Nav from "./Nav";
 import Header from "./Header";
 import Loader from "./Loader";
 
-class Homepage extends React.Component {
-  handleAnchorLink = () => {
-    // document.removeEventListener("scroll", this.handleParallax);
+const Homepage = props => {
+  const handleAnchorLink = () => {
     document
       .querySelector("div.homepage__casestudies-featured")
       .scrollIntoView({ behavior: "smooth", block: "start" });
   };
+  return (
+    <div>
+      {props.isLoading ? (
+        <Loader isLoading={props.isLoading} class="--dark-mode" />
+      ) : (
+        <div
+          className={`homepage__body ${
+            props.isLoading ? "--isLoading" : "--isLoaded"
+          }`}
+        >
+          <Nav page="homepage" class="--home" />
+          <section className="parallax--wrap">
+            <Header
+              isLoading={props.isLoading}
+              copy={
+                props.data
+                  ? props.data[0].data.home_page_header_title_copy[0].text
+                  : null
+              }
+              imageUrl={
+                props.data ? props.data[0].data.header_image_desktop.url : null
+              }
+              imageUrlMobile={
+                props.data[0].data.header_image_mobile.url
+                  ? props.data[0].data.header_image_mobile.url
+                  : props.data[0].data.header_image_desktop.url
+              }
+              autoplayUrl={
+                props.data[0].data.header_autoplay_vid.url
+                  ? props.data[0].data.header_autoplay_vid.url
+                  : null
+              }
+              autoplayUrlMobile={
+                props.data[0].data.header_autoplay_vid_mobile.url
+                  ? props.data[0].data.header_autoplay_vid_mobile.url
+                  : null
+              }
+              handleAnchorLink={handleAnchorLink}
+            />
 
-  // RENDER ---------------------------------------------
-  render() {
-    return (
-      <div>
-        {this.props.isLoading ? (
-          <Loader isLoading={this.props.isLoading} class="--dark-mode" />
-        ) : (
-          <div
-            className={`homepage__body ${
-              this.props.isLoading ? "--isLoading" : "--isLoaded"
-            }`}
-          >
-            <Nav page="homepage" class="--home" />
-            <section className="parallax--wrap">
-              <Header
-                isLoading={this.props.isLoading}
-                copy={
-                  this.props.data
-                    ? this.props.data[0].data.home_page_header_title_copy[0]
-                        .text
-                    : null
-                }
-                imageUrl={
-                  this.props.data
-                    ? this.props.data[0].data.header_image_desktop.url
-                    : null
-                }
-                imageUrlMobile={
-                  this.props.data[0].data.header_image_mobile.url
-                    ? this.props.data[0].data.header_image_mobile.url
-                    : this.props.data[0].data.header_image_desktop.url
-                }
-                autoplayUrl={
-                  this.props.data[0].data.header_autoplay_vid.url
-                    ? this.props.data[0].data.header_autoplay_vid.url
-                    : null
-                }
-                autoplayUrlMobile={
-                  this.props.data[0].data.header_autoplay_vid_mobile.url
-                    ? this.props.data[0].data.header_autoplay_vid_mobile.url
-                    : null
-                }
-                handleAnchorLink={this.handleAnchorLink}
-              />
-
-              <div className="homepage__casestudies-featured ">
-                {this.props.renderCasestudies()}
-              </div>
-            </section>
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+            <div className="homepage__casestudies-featured ">
+              {props.renderCasestudies()}
+            </div>
+          </section>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Homepage;
