740988a7bbd2c99dc6ab748213acb2a459e471bf
Kendall Strautman <kendallstraut@gmail.com>
2019-04-16 15:09:31 -0700

    added fade in to casestudy title

--- src/components/Slices/Title_Slice.js
+++ src/components/Slices/Title_Slice.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Waypoint } from "react-waypoint";
 
 const Title_Slice = props => {
+  const fadeIn = props.handleFadeIn(`${props.id}--title`);
+  const fadeOut = props.handleFadeOut(`${props.id}--title`);
   return (
-    <section className="casestudy__title">
-      <h1>{props.title}</h1>
+    <section id={`${props.id}--title`} className="casestudy__title animate">
+      <Waypoint onEnter={fadeIn} onLeave={fadeOut}>
+        <h1>{props.title}</h1>
+      </Waypoint>
     </section>
   );
 };
