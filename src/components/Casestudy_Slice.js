b6d1e3d2b401a2bd96af514d944fd5e9e1b1f1f0
Kendall Strautman <kendallstraut@gmail.com>
2019-04-23 14:01:32 -0700

    added audio support for other hosting platforms

--- src/components/Casestudy_Slice.js
+++ src/components/Casestudy_Slice.js
@@ -172,6 +172,7 @@ const Casestudy_Slice = props => {
           />
         );
       } else if (props.slice_type === "audio_module") {
+        console.log(props.slice_doc);
         return (
           <Audio_Module_Slice
             id={props.id}
@@ -180,8 +181,16 @@ const Casestudy_Slice = props => {
                 ? props.slice_doc.primary.audio_module_embed.embed_url
                 : null
             }
+            otherAudioUrl={
+              props.slice_doc.primary.audio_module_url.url
+                ? props.slice_doc.primary.audio_module_url.url
+                : null
+            }
             handleFadeIn={props.handleFadeIn}
             handleFadeOut={props.handleFadeOut}
+            isSoundcloud={
+              props.slice_doc.primary.embed_type == "is--other" ? false : true
+            }
           />
         );
       }
--- src/components/Slices/Audio_Module_Slice.js
+++ src/components/Slices/Audio_Module_Slice.js
@@ -3,29 +3,43 @@ import ReactPlayer from "react-player";
 import { Waypoint } from "react-waypoint";
 
 const Audio_Module_Slice = props => {
+  console.log(props);
   const fadeIn = props.handleFadeIn(props.id);
   const fadeOut = props.handleFadeOut(props.id);
   return (
-    <Waypoint onEnter={fadeIn} onLeave={fadeOut}>
-      <div id={props.id} className="audio-slice media--narrow">
-        <ReactPlayer
-          width="100%"
-          height="100%"
-          url={props.mediaModuleUrl}
-          config={{
-            soundcloud: {
-              options: {
-                width: "100%",
-                height: "100%",
-                visual: false,
-                show_artwork: false,
-                show_teaser: false
-              }
-            }
-          }}
-        />
-      </div>
-    </Waypoint>
+    <div>
+      {props.isSoundcloud ? (
+        <Waypoint onEnter={fadeIn} onLeave={fadeOut}>
+          <div id={props.id} className="audio-slice media--narrow">
+            <ReactPlayer
+              width="100%"
+              height="100%"
+              url={props.mediaModuleUrl}
+              config={{
+                soundcloud: {
+                  options: {
+                    width: "100%",
+                    height: "100%",
+                    visual: false,
+                    show_artwork: false,
+                    show_teaser: false
+                  }
+                }
+              }}
+            />
+          </div>
+        </Waypoint>
+      ) : (
+        <div id={props.id} className="audio-slice media--narrow">
+          <iframe
+            src={props.otherAudioUrl}
+            width="100%"
+            height="100%"
+            frameBorder="0"
+          />
+        </div>
+      )}
+    </div>
   );
 };
 
