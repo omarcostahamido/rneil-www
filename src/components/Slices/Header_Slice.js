8819cc50c6df05c06f95049d84b6ddde8f4a6d38
Kendall Strautman <kendallstraut@gmail.com>
2019-04-24 15:50:44 -0700

    added hook to calculate auto hero height in casestudy. fixed memory leak in casestudy_featured

--- src/components/Slices/Header_Slice.js
+++ src/components/Slices/Header_Slice.js
@@ -45,6 +45,7 @@ const Header_Slice = props => {
   };
   let [currentPath, updatePath] = useState(window.location.href);
   let [isFading, updateIsFading] = useState(false);
+  let [heroHeight, setHeroHeight] = useState(window.innerHeight);
   //to accommodate for in btw casestudy routing
   useEffect(() => {
     if (currentPath != window.location.href) {
@@ -60,6 +61,9 @@ const Header_Slice = props => {
   useEffect(() => {
     if (!infoIsVisible) {
       fadeIn();
+    }
+    if (window.innerWidth >= 1280) {
+      setHeroHeight(null);
     }
   }, []);
   return (
@@ -77,7 +81,7 @@ const Header_Slice = props => {
         style={
           !props.isVideo
             ? {
-                height: window.innerWidth <= 1024 && window.innerHeight,
+                height: heroHeight,
                 backgroundColor: "inherit",
                 backgroundImage: `url(${props.casestudyHero})`,
                 WebkitBackgroundSize: "cover",
@@ -88,7 +92,7 @@ const Header_Slice = props => {
               }
             : {
                 backgroundColor: "transparent",
-                height: window.innerWidth <= 1024 && window.innerHeight
+                height: heroHeight
               }
         }
       >
@@ -132,3 +136,140 @@ const Header_Slice = props => {
 };
 
 export default Header_Slice;
+
+// import React, { useEffect, useState } from "react";
+// import Autoplay_Video_Module from "./Autoplay_Video_Module";
+// import Down_Arrow from "../Down_Arrow";
+// import { Waypoint } from "react-waypoint";
+
+// const Header_Slice = props => {
+//   const fadeIn = () => {
+//     document.querySelector(".header-slice__info").classList.add("is--active");
+//     document.querySelector(".header-slice__info").classList.remove("animate");
+//     setInfoVisible(true);
+//   };
+//   const fadeOut = () => {
+//     if (infoIsVisible) {
+//       document
+//         .querySelector(".header-slice__info")
+//         .classList.remove("is--active");
+//       document.querySelector(".header-slice__info").classList.add("animate");
+//       setInfoVisible(false);
+//     }
+//   };
+//   const fadeInBg = () => {
+//     updateIsFading(() => {
+//       isFading = true;
+//     });
+//     document.querySelector(".casestudy__header").classList.add("is--active");
+//     document.querySelector(".casestudy__header").classList.remove("animate");
+//     updateIsFading(() => {
+//       isFading = false;
+//     });
+//   };
+//   const fadeOutBg = () => {
+//     updateIsFading(() => {
+//       isFading = true;
+//     });
+//     document.querySelector(".casestudy__header").classList.remove("is--active");
+//     document.querySelector(".casestudy__header").classList.add("animate");
+//     updateIsFading(() => {
+//       isFading = false;
+//     });
+//   };
+//   const handleAnchorScroll = () => {
+//     document
+//       .querySelector(".casestudy__location-info")
+//       .scrollIntoView({ behavior: "smooth", block: "start" });
+//   };
+//   let [currentPath, updatePath] = useState(window.location.href);
+//   let [isFading, updateIsFading] = useState(false);
+//   let [heroHeight, setHeroHeight] = useState(window.innerHeight);
+//   //to accommodate for in btw casestudy routing
+//   useEffect(() => {
+//     console.log(heroHeight);
+//     if (currentPath != window.location.href) {
+//       updatePath(() => {
+//         currentPath = window.location.href;
+//       });
+//       if (!isFading) {
+//         fadeOutBg();
+//       }
+//     }
+//   });
+//   let [infoIsVisible, setInfoVisible] = useState(false);
+//   useEffect(() => {
+//     if (!infoIsVisible) {
+//       fadeIn();
+//     }
+//   }, []);
+//   return (
+//     <header
+//       className="--isLoaded"
+//       onClick={window.innerWidth > 1024 ? handleAnchorScroll : undefined}
+//     >
+//       <img
+//         src={props.casestudyHero}
+//         onLoad={fadeInBg}
+//         style={{ display: "none" }}
+//       />
+//       <figure
+//         className="casestudy__header animate"
+//         style={
+//           !props.isVideo
+//             ? {
+//                 height: { heroHeight },
+//                 backgroundColor: "inherit",
+//                 backgroundImage: `url(${props.casestudyHero})`,
+//                 WebkitBackgroundSize: "cover",
+//                 MozBackgroundSize: "cover",
+//                 backgroundSize: "cover",
+//                 backgroundRepeat: "no-repeat",
+//                 backgroundPosition: "center"
+//               }
+//             : {
+//                 backgroundColor: "transparent",
+//                 height: window.innerWidth <= 1024 ? window.innerHeight : null
+//               }
+//         }
+//       >
+//         {props.isVideo ? (
+//           <Autoplay_Video_Module
+//             type="header"
+//             autoplayVideoUrl={props.casestudyHeroVideo}
+//             style="autoplay-hero"
+//             handleFadeIn={props.handleFadeIn}
+//             handleFadeOut={props.handleFadeOut}
+//           />
+//         ) : null}
+//         <article
+//           className="header-slice__info"
+//           style={
+//             props.isVideo
+//               ? { position: "absolute", zIndex: "2", bottom: "0", left: "0" }
+//               : null
+//           }
+//         >
+//           <Waypoint
+//             onEnter={fadeIn}
+//             onLeave={fadeOut}
+//             topOffset={window.innerWidth < 768 ? "40%" : "60%"}
+//           />
+//           <h1
+//             id={`${props.id}--titleCopy`}
+//             className={props.titleCopyColor ? props.titleCopyColor : null}
+//           >
+//             {props.titleCopy}
+//           </h1>
+
+//           <Down_Arrow
+//             class="down-arrow"
+//             titleCopyColor={props.titleCopyColor}
+//           />
+//         </article>
+//       </figure>
+//     </header>
+//   );
+// };
+
+// export default Header_Slice;
